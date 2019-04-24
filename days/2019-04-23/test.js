2fe76c0bbf48725c3fb14202b160a9063eaaf5b1
Joel Edwards <joeledwards@gmail.com>
2019-04-24 05:54:11 +0000

    feat: updated tests for 2019-04-23

--- days/2019-04-23/test.js
+++ days/2019-04-23/test.js
@@ -5,20 +5,9 @@ const problemDay = path.basename(__dirname)
 
 tap.test(`solution for ${problemDay} should work`, async assert => {
   assert.same(t(), d(s(t())))
-  console.log(s(t()))
-
   assert.same(t('root'), d(s(t('root'))))
-  console.log(s(t('root')))
-
   assert.same(t('root', t('left')), d(s(t('root', t('left')))))
-  console.log(s(t('root', t('left'))))
-
   assert.same(t('root', null, t('right')), d(s(t('root', null, t('right')))))
-  console.log(s(t('root', null, t('right'))))
-
   assert.same(t('root', t('left'), t('right')), d(s(t('root', t('left'), t('right')))))
-  console.log(s(t('root', t('left'), t('right'))))
-
   assert.same(t('root', t('left', t('left.left')), t('right')), d(s(t('root', t('left', t('left.left')), t('right')))))
-  console.log(s(t('root', t('left', t('left.left')), t('right'))))
 })
