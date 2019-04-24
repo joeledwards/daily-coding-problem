ad1be091dcd2afaf414dcedd3c286d520e4df865
Joel Edwards <joeledwards@gmail.com>
2019-04-24 06:11:52 +0000

    feat: added test for 2019-04-23

--- days/2019-04-23/test.js
+++ days/2019-04-23/test.js
@@ -10,4 +10,5 @@ tap.test(`solution for ${problemDay} should work`, async assert => {
   assert.same(t('root', null, t('right')), d(s(t('root', null, t('right')))))
   assert.same(t('root', t('left'), t('right')), d(s(t('root', t('left'), t('right')))))
   assert.same(t('root', t('left', t('left.left')), t('right')), d(s(t('root', t('left', t('left.left')), t('right')))))
+  assert.equals(d(s(t('root', t('left', t('left.left')), t('right')))).left.left.val, 'left.left')
 })
